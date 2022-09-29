a385e3e3ccb34290d94294da189e2a063c2d6077
AiHKhaled <aithammouk40@gmail.com>
2022-09-29 01:34:11 +0100

    added services section

---
+++ src/components/_Job.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    export let image: string;
+    export let title: string;
+    export let description: string;
+</script>
+
+<div class="job">
+    <img src={image} class="w-10" alt="" />
+    <h4 class="font-semibold">{title}</h4>
+    <p class="desc text-slate-500">{description}</p>
+</div>
+
+<style>
+    .job {
+        width: 300px;
+        height: 270px;
+        border: 4px solid rgb(148 163 184);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        row-gap: 15px;
+        padding: 20px;
+    }
+    .desc {
+        text-align: center;
+        padding-left: 20px;
+    }
+</style>
--- src/components/_Services.svelte
+++ src/components/_Services.svelte
@@ -1,21 +1,63 @@
-<div class=" container ">
-    <h1 class="tracking-widest text-slate-400 md: text-4xl text-right">Services</h1>
+<script lang="ts">
+    import Job from './_Job.svelte';
+    import camera from '$lib/static/camera.svg';
+    import picture from '$lib/static/picture.svg';
+    import video from '$lib/static/video.svg';
 
-    <p class="pr-10 text-right">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae in quam, vel animi
-        accusamus, delectus cum harum aliquid corrupti enim ipsum quis, doloribus odit quae deserunt
-        temporibus porro voluptates. Nobis.
-    </p>
-</div>
+    let jobs: { id: number; image: string; title: string; description: string }[] = [
+        {
+            id: 1,
+            image: camera,
+            title: 'Photo Shooting',
+            description:
+                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, eum eius'
+        },
+        {
+            id: 2,
+            image: picture,
+            title: 'Retouch',
+            description:
+                ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, eum eius nulla praesentium alias blanditiis.'
+        },
+
+        {
+            id: 3,
+            image: video,
+            title: 'Video Shooting',
+            description:
+                ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, eum eius nulla praesentium alias blanditiis.'
+        }
+    ];
+</script>
+
+<section class=" container mt-[50px] flex lg:w-[100%] justify-between ">
+    <div class="lg:justify-center flex  lg:flex-col  flex-wrap w-[200px]">
+        {#each jobs as job}
+            {#if job.id % 2 != 0}
+                <div class=" lg:mx-[400px]  ">
+                    <Job image={job.image} title={job.title} description={job.description} />
+                </div>
+            {:else if job.id % 2 == 0}
+                <div class="sm:m-0">
+                    <Job image={job.image} title={job.title} description={job.description} />
+                </div>
+            {/if}
+        {/each}
+    </div>
+    <div class="lg:w-[500px] lg:align-center flex  md:flex-col lg:float-right">
+        <h4 class="tracking-widest text-slate-400  md: text-4xl text-right">Services</h4>
+
+        <p class=" invisible lg:visible pr-24 text-right">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae in quam, vel animi
+            accusamus, delectus cum harum aliquid corrupti enim ipsum quis, doloribus odit quae
+            deserunt temporibus porro voluptates. Nobis.
+        </p>
+    </div>
+</section>
 
 <style>
     .container {
-        width: 400px;
-        margin-top: 50px;
-        display: flex;
-        flex-direction: column;
-        border: 1px solid red;
         gap: 20px;
-        float: right;
+        padding-left: 60px;
     }
 </style>
